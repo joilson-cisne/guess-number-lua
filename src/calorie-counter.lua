28a884656be9fcf6c2e8af0bbd154826aca58969
Joilson Cisne <joilsoncisne@gmail.com>
2018-06-15 17:55:34 -0300

    Extrac functionalities in modules

--- src/calorie-counter.lua
+++ src/calorie-counter.lua
@@ -19,16 +19,49 @@ function CalorieCounter:new(t)
 end
 
 -- c = CalorieCounter:new({ goal = 1500 })
-c = CalorieCounter:new{ goal = 1500 }
-c:add(500)
-print(c.count)
-print(c:didReachGoal())
+-- c = CalorieCounter:new{ goal = 1500 }
+-- c:add(500)
+-- print(c.count)
+-- print(c:didReachGoal())
 
-c:add(1000)
-print(c.count)
-print(c:didReachGoal())
+-- c:add(1000)
+-- print(c.count)
+-- print(c:didReachGoal())
 
 
 -- The same as...
 -- function CalorieCounter.add(self, amount)
 -- end
+
+
+----------------
+HealthMonitor = CalorieCounter:new{ steps = 0, stepGoal = 10000 }
+
+function HealthMonitor:step(amount)
+  self.steps = self.steps + amount
+end
+
+function HealthMonitor:didReachGoal()
+  local goalsReached = 0
+
+  if self.count >= self.goal then
+    print('Calorie goal of ' .. self.count .. ' reached')
+    goalsReached = goalsReached + 1
+  else
+    print('Calorie goal not reached, yet')
+  end
+
+  if self.steps >= self.stepGoal then
+    print('Step goal of ' .. self.stepGoal .. ' reached')
+    goalsReached = goalsReached + 1
+  else
+    print('Step goal not reached, yet')
+  end
+
+  return goalsReached >= 2
+end
+
+return {
+  CalorieCounter = CalorieCounter,
+  HealthMonitor = HealthMonitor,
+}
